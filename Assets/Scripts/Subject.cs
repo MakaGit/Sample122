e16e8e537a0f1f8fb061b27f0ebd1358e7b0f2d2
Emod <andrey.makarenok01@gmail.com>
2020-03-13 19:07:57 +0300

    Resolution fix

--- Assets/Scripts/Subject.cs
+++ Assets/Scripts/Subject.cs
@@ -6,24 +6,33 @@ using UnityEngine.UI;
 using TMPro;
 
 public class Subject : MonoBehaviour
-{
-    [SerializeField] public Vector3 endPosition;
+{   
     [SerializeField] public float duration = 1.0f;
     [SerializeField] private TMP_Text _text;
     [SerializeField] private Image _imageChild;
     private Image _image;
+    //private Vector3 endPositionloc;
+    //private Vector3 startPositionloc;
+    public Vector3 endPosition;
     public Vector3 startPosition;
     private bool _flag;
 
-    private void Start()
+    private void Awake()
     {
-        startPosition = transform.position;
+
+        //startPosition = transform.position;
+        //endPosition = transform.InverseTransformPoint(transform.);
+        //endPosition = transform.position;
+
         _image = GetComponent<Image>();
 
         _image.DOFade(0.0f, 0.0f);
         _text.DOFade(0.0f, 0.0f);
         _imageChild.DOFade(0.0f, 0.0f);
 
+    }
+    private void Start()
+    {
 
         //var Seq = DOTween.Sequence();
 
@@ -47,14 +56,14 @@ public class Subject : MonoBehaviour
 
         var Seq = DOTween.Sequence();
 
-        Seq.Append(transform.DOMove(endPosition, duration, false));
+        Seq.Append(transform.DOLocalMoveX(endPosition.x, duration, false));
         Seq.Join(_image.DOFade(1.0f, duration));
         Seq.Join(_imageChild.DOFade(1.0f, duration));
         Seq.Join(_text.DOFade(1.0f, duration));
 
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
 
-        Seq.Append(transform.DOMove(startPosition, duration, false));
+        Seq.Append(transform.DOLocalMoveX(startPosition.x, duration, false));
         Seq.Join(_image.DOFade(0.0f, duration));
         Seq.Join(_imageChild.DOFade(0.0f, duration));
         Seq.Join(_text.DOFade(0.0f, duration));
