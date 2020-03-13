75978c0372c01f49a1989fb3abb908e67427b7d1
Emod <andrey.makarenok01@gmail.com>
2020-03-13 07:54:38 +0300

    no message

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    [SerializeField] private Subject _mainSub;
+    [SerializeField] private Subject _leftSub;
+    [SerializeField] private Subject _rightSub;
+
+    private Queue<IEnumerator> Scenario = new Queue<IEnumerator>();
+
+    private void Start()
+    {
+        //StartCoroutine(_leftSub.Step("Test", _leftSub.duration, _leftSub.endPosition, _leftSub.startPosition));
+        //StartCoroutine(_rightSub.Step("Test", _rightSub.duration, _rightSub.endPosition, _rightSub.startPosition));
+        //StartCoroutine(_mainSub.Step("Test", _mainSub.duration, _mainSub.endPosition, _mainSub.startPosition));
+
+    }
+
+}
---
+++ Assets/Scripts/Subject.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+
+public class Subject : MonoBehaviour
+{
+    [SerializeField] public Vector3 endPosition;
+    [SerializeField] public float duration = 1.0f;
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Image _imageChild;
+    private Image _image;
+    public Vector3 startPosition;
+    private bool _flag;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        _image = GetComponent<Image>();
+
+        _image.DOFade(0.0f, 0.0f);
+        _text.DOFade(0.0f, 0.0f);
+        _imageChild.DOFade(0.0f, 0.0f);
+
+
+        //var Seq = DOTween.Sequence();
+
+        //    Seq.Append(transform.DOMove(endPosition, duration, false));
+        //    Seq.Join(_image.DOFade(1.0f, duration));
+        //    Seq.Join(_imageChild.DOFade(1.0f, duration));
+        //    Seq.Join(_text.DOFade(1.0f, duration));
+        //    Seq.AppendInterval(1.0f);
+
+        //    Seq.Append(transform.DOMove(startPosition, duration, false));
+        //    Seq.Join(_image.DOFade(0.0f, duration));
+        //    Seq.Join(_imageChild.DOFade(0.0f, duration));
+        //    Seq.Join(_text.DOFade(0.0f, duration));
+        //    Seq.AppendInterval(1.0f);
+
+    }
+
+    public IEnumerator Step(string str, float duration, Vector3 endPosition, Vector3 startPosition)
+    {
+        _text.text = str;
+
+        var Seq = DOTween.Sequence();
+
+        Seq.Append(transform.DOMove(endPosition, duration, false));
+        Seq.Join(_image.DOFade(1.0f, duration));
+        Seq.Join(_imageChild.DOFade(1.0f, duration));
+        Seq.Join(_text.DOFade(1.0f, duration));
+
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+
+        Seq.Append(transform.DOMove(startPosition, duration, false));
+        Seq.Join(_image.DOFade(0.0f, duration));
+        Seq.Join(_imageChild.DOFade(0.0f, duration));
+        Seq.Join(_text.DOFade(0.0f, duration));
+
+        yield return null;
+
+    }
+}
